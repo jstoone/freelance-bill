612673bd872413ad768226cd78cfce257006ff1a
Jakob Steinn <js@contentcube.dk>
2014-11-26 20:21:41 +0100

    Example environment file.

---
+++ .env.example.php
@@ -0,0 +1,9 @@
+<?php
+return [
+	"STRIPE_SECRET_KEY"     => "[insert_stripe_secret_key]",
+	"STRIPE_PUBLISH_KEY"    => "[insert_stripe_publishable_key]",
+	'DB_USER'               => '[database_user]',
+	'DB_PASS'               => '[database_password]',
+	'DB_NAME'               => '[database_name]',
+];
+
