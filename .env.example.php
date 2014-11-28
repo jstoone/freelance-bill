f1b3bed8a2b0b8d2bf6f631f87962ced24c4725f
Jakob Steinn <js@contentcube.dk>
2014-11-28 18:51:13 +0100

    Added mandril secret key to .env.

--- .env.example.php
+++ .env.example.php
@@ -1,7 +1,8 @@
 <?php
 return [
-	"STRIPE_SECRET_KEY"     => "[insert_stripe_secret_key]",
-	"STRIPE_PUBLISH_KEY"    => "[insert_stripe_publishable_key]",
+	'STRIPE_SECRET_KEY'     => '[insert_stripe_secret_key]',
+	'STRIPE_PUBLISH_KEY'    => '[insert_stripe_publishable_key]',
+	'MANDRILL_SECRET_KEY'   => '[insert_mailgun_secret_key]',
 	'DB_USER'               => '[database_user]',
 	'DB_PASS'               => '[database_password]',
 	'DB_NAME'               => '[database_name]',
